2f748ed62514ffa260396dcaa037d3fff3eedcd4
Gorsing  Pawar <adeshp001@gmail.com>
2024-10-25 11:45:58 +0530

    Day 2 of Java

---
+++ area2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class area2 {
+    public static void main(String[] args) {
+        
+    
+    Scanner sc = new Scanner(System.in);
+    float rad= sc.nextInt();
+    float area =3.14f * rad * rad;/*f Stands for float we need to take 3.14 as a float so we used :"f" */
+    System.out.println(area);
+
+    }
+}
---
+++ javabasics.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class javabasics{
+    public static void main(String[] args) {
+        // System.err.println("hellow World");
+        // System.out.print("****\n***\n**\n*");
+        /* *
+        int a = 10;
+        int b = 5;
+        int c = a*b;
+        System.out.println(c);
+        /* */
+        Scanner a = new Scanner(System.in);
+        int f = a.nextInt();
+        int g = a.nextInt();
+        int c = f*g;
+        System.err.println(c);
+        
+    }
+}
