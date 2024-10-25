bbe20ebbc8f89f1c43db654b8fa6e553e554d420
Gorsing  Pawar <adeshp001@gmail.com>
2024-10-25 23:48:35 +0530

    day 2 night session

---
+++ javabasics/charvalue.java
@@ -0,0 +1,12 @@
+package javabasics;
+
+public class charvalue {
+    public static void main(String[] args) {
+      
+        System.out.println("ch");
+        
+
+
+    }
+    
+}
---
+++ typecast.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+public class typecast {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        // float marks =83.55f;
+        // int marks2=(int)marks;
+        // System.out.println(marks2);        
+    
+
+    }
+    
+}
