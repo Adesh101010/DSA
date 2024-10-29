1400a728948d5289a1782849fe6616f7eee0bf89
Gorsing  Pawar <adeshp001@gmail.com>
2024-10-29 09:58:38 +0530

    changed folder

--- basics/area2.java
+++ basics/area2.java
@@ -1,5 +1,8 @@
+package basics;
+
 import java.util.*;
-public class area2 {
+
+ class area2 {
     public static void main(String[] args) {
         
     
@@ -7,6 +10,7 @@ public class area2 {
     float rad= sc.nextInt();
     float area =3.14f * rad * rad;/*f Stands for float we need to take 3.14 as a float so we used :"f" */
     System.out.println(area);
+    
 
     }
 }
--- basics/typecast.java
+++ basics/typecast.java
@@ -4,9 +4,10 @@ import java.util.*;
 public class typecast {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        // float marks =83.55f;
-        // int marks2=(int)marks;
-        // System.out.println(marks2);        
+        float marks =83.55f;
+        int marks2=(int)marks;
+        System.out.println(marks2);    
+            
     
 
     }
